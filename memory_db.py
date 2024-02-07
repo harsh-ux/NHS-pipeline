8842d913db045db44509884bc1466b9821d22d32
Reinis Barkans <rb2123@ic.ac.uk>
2024-02-07 19:40:46 +0000

    Implemented further databse functionality

--- memory_db.py
+++ memory_db.py
@@ -1,17 +1,35 @@
 import sqlite3
+from constants import ON_DISK_DB_PATH
+import os
 
 class InMemoryDatabase():
     def __init__(self):
         self.connection = sqlite3.connect(':memory:')
-        self.initialise_table()
+        self.initialise_tables()
     
 
-    def initialise_table(self):
+    def initialise_tables(self):
         """
-        Initialises the database with the patient features table.
+        Initialise the database with the patient features table.
         """
-        query = """
+        create_patients = """
             CREATE TABLE patients (
+                mrn TEXT PRIMARY KEY,   
+                age INTEGER,
+                sex TEXT
+            );
+        """
+        create_test_results = """
+            CREATE TABLE test_results (
+                mrn TEXT,   
+                date DATETIME,
+                result DECIMAL,
+                PRIMARY KEY (mrn, date),
+                FOREIGN KEY (mrn) REFERENCES patients (mrn)
+            );
+        """
+        create_patient_features = """
+            CREATE TABLE features (
                 mrn TEXT PRIMARY KEY,   
                 age INTEGER,
                 sex TEXT,
@@ -25,13 +43,15 @@ class InMemoryDatabase():
                 aki TEXT
             );
         """
-        # features table
-        self.connection.execute(query)
+        # create the tables
+        self.connection.execute(create_patients)
+        self.connection.execute(create_test_results)
+        self.connection.execute(create_patient_features)
 
 
-    def insert_patient(self, mrn, age, sex, c1, rv1, rv1_r, rv2, rv2_r, change, D, aki=None):
+    def insert_patient_features(self, mrn, age, sex, c1, rv1, rv1_r, rv2, rv2_r, change, D, aki=None):
         """
-        Inserts the obtained features into the in-memory database.
+        Insert the obtained features into the in-memory database.
         Args:
             - mrn {str}: Medical Record Number of the patient
             - age {int}: Age of the patient
@@ -45,49 +65,178 @@ class InMemoryDatabase():
             - D {float}: Difference between current and lowest previous result (48h)
             - aki {str}: Whether the patient has been diagnosed with aki ('y'/'n')
         """
-
         query = """
-            INSERT INTO patients 
+            INSERT INTO features 
                 (mrn, age, sex, C1, RV1, RV1_ratio, RV2, RV2_ratio, has_changed_48h, D, aki) 
             VALUES 
                 (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         """
-
+        # execute the query
         self.connection.execute(
             query,
             (mrn, age, sex, c1, rv1, rv1_r, rv2, rv2_r, change, D, aki)
         )
         self.connection.commit()
 
+    
+    def insert_patient(self, mrn, age, sex):
+        """
+        Insert the patient info from PAS into the in-memory database.
+        Args:
+            - mrn {str}: Medical Record Number of the patient
+            - age {int}: Age of the patient
+            - sex {str}: Sex of the patient ('m'/'f')
+        """
+        query = """
+            INSERT INTO patients 
+                (mrn, age, sex) 
+            VALUES 
+                (?, ?, ?)
+        """
+        # execute the query
+        self.connection.execute(
+            query,
+            (mrn, age, sex)
+        )
+        self.connection.commit()
+
+
+    def insert_test_result(self, mrn, date, result):
+        """
+        Insert the patient info from PAS into the in-memory database.
+        Args:
+            - mrn {str}: Medical Record Number of the patient
+            - date {datetime}: creatinine result date
+            - result {float}: creatinine result
+        """
+        query = """
+            INSERT INTO test_results 
+                (mrn, date, result) 
+            VALUES 
+                (?, ?, ?)
+        """
+        # execute the query
+        self.connection.execute(
+            query,
+            (mrn, date, result)
+        )
+        self.connection.commit()
+
+
+    def get_patient_features(self, mrn):
+        """
+        Query the features table for a given mrn.
+        Args:
+            - mrn {str}: Medical Record Number
+        """
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT * FROM features WHERE mrn = ?', (mrn,))
+        return cursor.fetchone()
+    
 
     def get_patient(self, mrn):
         """
-        Query the patient data for a given mrn.
+        Query the patients table for a given mrn.
+        Args:
+            - mrn {str}: Medical Record Number
         """
-
         cursor = self.connection.cursor()
         cursor.execute('SELECT * FROM patients WHERE mrn = ?', (mrn,))
         return cursor.fetchone()
     
 
-    def update_patient(self, mrn, **kwargs):
+    def get_test_results(self, mrn):
+        """
+        Query the test results table for a given mrn.
+        Args:
+            - mrn {str}: Medical Record Number
+        """
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT * FROM test_results WHERE mrn = ?', (mrn,))
+        return cursor.fetchall()
+    
+
+    def get_patient_history(self, mrn):
+        """
+        Get patient info along with all their test results and their dates.
+        Args:
+            - mrn {str}: Medical Record Number
+        Returns:
+            - _ {list}: List of records
+        """
+        query = """
+            SELECT 
+                * 
+            FROM
+                patients
+            JOIN
+                test_results 
+            ON
+                patients.mrn = test_results.mrn
+            WHERE patients.mrn = ?
+        """
+        cursor = self.connection.cursor()
+        cursor.execute(query, (mrn,))
+        return cursor.fetchall()
+    
+
+    def discharge_patient(self, mrn, update_disk_db=True):
+        """
+        Remove the patient record from patients table in-memory and on-disk. Test 
+        results are kept in the test_results table for historic data.
+        Args:
+            - mrn {str}: Medical Record Number
+        """
+        # delete from in-memory
+        self.connection.execute('DELETE FROM patients WHERE mrn = ?', (mrn,))
+        self.connection.commit()
+        # delete from on-disk
+        if update_disk_db:
+            disk_conn = sqlite3.connect(ON_DISK_DB_PATH)
+            disk_conn.execute('DELETE FROM patients WHERE mrn = ?', (mrn,))
+            disk_conn.commit()
+            disk_conn.close()
+    
+
+    def update_patient_features(self, mrn, **kwargs):
         """
         Update patient information based on the provided keyword arguments.
         Args:
             - mrn {str}: Medical Record Number of the patient to update
             - **kwargs {dict}: Where key=column, value=new value
         """
-
         # construct the SET part of the SQL query based on the given args
-        set_clause = ", ".join([f"{key} = ?" for key in kwargs])
-        query = f"UPDATE patients SET {set_clause} WHERE mrn = ?"
-
+        set_clause = ', '.join([f"{key} = ?" for key in kwargs])
+        query = f'UPDATE features SET {set_clause} WHERE mrn = ?'
         # prepare the values for the placeholders in the SQL statement
         values = list(kwargs.values()) + [mrn]
-
-        # execute the update query
+        # execute the query
         self.connection.execute(query, values)
         self.connection.commit()
+
+    
+    def persist_db(self):
+        """
+        Persist the in-memory database to disk.
+        Args:
+            - disk_db_path {str}: the path to the database
+        """
+        # create an empty db file if it does not exist already
+        if not os.path.exists(ON_DISK_DB_PATH):
+            with open(ON_DISK_DB_PATH, 'w'):
+                pass
+        # connect to the disk db
+        disk_connection = sqlite3.connect(ON_DISK_DB_PATH, check_same_thread=False)
+        # backs up and closes the connection
+        with disk_connection:
+            self.connection.backup(disk_connection)
+
+
+    def load_db(self):
+        """
+        Load the on-disk database into the in-memory database.
+        """
+        pass
     
 
     def close(self):
