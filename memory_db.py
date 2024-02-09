51c60892ba199bb9a16c46e9ed3179d454a73d7c
RohitMidha23 <rohit.midha23@gmail.com>
2024-02-09 13:32:00 +0000

    updated the docker file code to pick up history from data/history

--- memory_db.py
+++ memory_db.py
@@ -3,15 +3,15 @@ from constants import ON_DISK_DB_PATH
 import os
 from utils import populate_test_results_table, populate_patients_table
 
-class InMemoryDatabase():
+
+class InMemoryDatabase:
     def __init__(self):
-        self.connection = sqlite3.connect(':memory:')
+        self.connection = sqlite3.connect(":memory:")
         self.initialise_tables()
         self.load_db()
         # make sure we always have a db file
         if not os.path.exists(ON_DISK_DB_PATH):
             self.persist_db()
-    
 
     def initialise_tables(self):
         """
@@ -53,8 +53,9 @@ class InMemoryDatabase():
         self.connection.execute(create_test_results)
         self.connection.execute(create_patient_features)
 
-
-    def insert_patient_features(self, mrn, age, sex, c1, rv1, rv1_r, rv2, rv2_r, change, D, aki=None):
+    def insert_patient_features(
+        self, mrn, age, sex, c1, rv1, rv1_r, rv2, rv2_r, change, D, aki=None
+    ):
         """
         Insert the obtained features into the in-memory database.
         Args:
@@ -79,13 +80,12 @@ class InMemoryDatabase():
         # execute the query
         try:
             self.connection.execute(
-                query,
-                (mrn, age, sex, c1, rv1, rv1_r, rv2, rv2_r, change, D, aki)
+                query, (mrn, age, sex, c1, rv1, rv1_r, rv2, rv2_r, change, D, aki)
             )
             self.connection.commit()
         except sqlite3.IntegrityError:
-            print(f'The features for patient {mrn} are already in the features table!')
-    
+            print(f"The features for patient {mrn} are already in the features table!")
+
     def insert_patient(self, mrn, age, sex, update_disk_db=True):
         """
         Insert the patient info from PAS into the in-memory database.
@@ -102,21 +102,17 @@ class InMemoryDatabase():
         """
         # execute the query
         try:
-            self.connection.execute(
-                query,
-                (mrn, age, sex)
-            )
+            self.connection.execute(query, (mrn, age, sex))
             self.connection.commit()
-            
+
         except sqlite3.IntegrityError:
-            print(f'Patient {mrn} is already in the patients table!')
-        
+            print(f"Patient {mrn} is already in the patients table!")
+
         # if update_disk_db:
         #     disk_conn = sqlite3.connect(ON_DISK_DB_PATH)
         #     disk_conn.execute('INSERT OR IGNORE INTO patients (mrn, age, sex) VALUES (?, ?, ?)', (mrn, age, sex))
         #     disk_conn.commit()
         #     disk_conn.close()
-
 
     def insert_test_result(self, mrn, date, result):
         """
@@ -134,14 +130,12 @@ class InMemoryDatabase():
         """
         # execute the query
         try:
-            self.connection.execute(
-                query,
-                (mrn, date, result)
-            )
+            self.connection.execute(query, (mrn, date, result))
             self.connection.commit()
         except sqlite3.IntegrityError:
-            print(f'Test result on date-time: {date} for: {mrn} is already in the test_results table!')
-
+            print(
+                f"Test result on date-time: {date} for: {mrn} is already in the test_results table!"
+            )
 
     def get_patient_features(self, mrn):
         """
@@ -150,9 +144,8 @@ class InMemoryDatabase():
             - mrn {str}: Medical Record Number
         """
         cursor = self.connection.cursor()
-        cursor.execute('SELECT * FROM features WHERE mrn = ?', (mrn,))
+        cursor.execute("SELECT * FROM features WHERE mrn = ?", (mrn,))
         return cursor.fetchone()
-    
 
     def get_patient(self, mrn):
         """
@@ -161,9 +154,8 @@ class InMemoryDatabase():
             - mrn {str}: Medical Record Number
         """
         cursor = self.connection.cursor()
-        cursor.execute('SELECT * FROM patients WHERE mrn = ?', (mrn,))
+        cursor.execute("SELECT * FROM patients WHERE mrn = ?", (mrn,))
         return cursor.fetchone()
-    
 
     def get_test_results(self, mrn):
         """
@@ -172,9 +164,8 @@ class InMemoryDatabase():
             - mrn {str}: Medical Record Number
         """
         cursor = self.connection.cursor()
-        cursor.execute('SELECT * FROM test_results WHERE mrn = ?', (mrn,))
+        cursor.execute("SELECT * FROM test_results WHERE mrn = ?", (mrn,))
         return cursor.fetchall()
-    
 
     def get_patient_history(self, mrn):
         """
@@ -202,24 +193,22 @@ class InMemoryDatabase():
         cursor = self.connection.cursor()
         cursor.execute(query, (mrn,))
         return cursor.fetchall()
-    
 
     def discharge_patient(self, mrn, update_disk_db=True):
         """
-        Remove the patient record from patients table in-memory and on-disk. Test 
+        Remove the patient record from patients table in-memory and on-disk. Test
         results are kept in the test_results table for historic data.
         Args:
             - mrn {str}: Medical Record Number
         """
         # delete from in-memory
-        self.connection.execute('DELETE FROM patients WHERE mrn = ?', (mrn,))
+        self.connection.execute("DELETE FROM patients WHERE mrn = ?", (mrn,))
         self.connection.commit()
         # delete from on-disk
         if update_disk_db:
             with sqlite3.connect(ON_DISK_DB_PATH) as disk_connection:
-                disk_connection.execute('DELETE FROM patients WHERE mrn = ?', (mrn,))
+                disk_connection.execute("DELETE FROM patients WHERE mrn = ?", (mrn,))
                 disk_connection.commit()
-    
 
     def update_patient_features(self, mrn, **kwargs):
         """
@@ -229,15 +218,14 @@ class InMemoryDatabase():
             - **kwargs {dict}: Where key=column, value=new value
         """
         # construct the SET part of the SQL query based on the given args
-        set_clause = ', '.join([f"{key} = ?" for key in kwargs])
-        query = f'UPDATE features SET {set_clause} WHERE mrn = ?'
+        set_clause = ", ".join([f"{key} = ?" for key in kwargs])
+        query = f"UPDATE features SET {set_clause} WHERE mrn = ?"
         # prepare the values for the placeholders in the SQL statement
         values = list(kwargs.values()) + [mrn]
         # execute the query
         self.connection.execute(query, values)
         self.connection.commit()
 
-    
     def persist_db(self):
         """
         Persist the in-memory database to disk.
@@ -248,28 +236,23 @@ class InMemoryDatabase():
         with sqlite3.connect(ON_DISK_DB_PATH) as disk_connection:
             self.connection.backup(disk_connection)
 
-
     def load_db(self):
         """
         Load the on-disk database into the in-memory database.
         """
         # if on-disk db doesn't exist, use the csv file
         if not os.path.exists(ON_DISK_DB_PATH):
-            print('Loading the history.csv file in memory.')
-            populate_test_results_table(self, 'history.csv')
-            #populate_patients_table(self, 'processed_history.csv')
+            print("Loading the history.csv file in memory.")
+            populate_test_results_table(self, "data/history.csv")
+            # populate_patients_table(self, 'processed_history.csv')
         else:
             # load the on-disk db into the in-memory one
             with sqlite3.connect(ON_DISK_DB_PATH) as disk_connection:
-                print('Loading the on-disk database in memory.')
+                print("Loading the on-disk database in memory.")
                 disk_connection.backup(self.connection)
 
-    
     def close(self):
         """
         Close the database connection.
         """
         self.connection.close()
-
-    
-    
