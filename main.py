5a4362664cfb46480ffd350dae6e9b6ca6686bfa
RohitMidha23 <rohit.midha23@gmail.com>
2024-02-09 12:30:41 +0000

    updated code to work with docker

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import socket
+import argparse
 from joblib import load
 from utils import (
     process_mllp_message,
     parse_hl7_message,
     create_acknowledgement,
     parse_system_message,
+    strip_url,
 )
 from memory_db import InMemoryDatabase
-from constants import DT_MODEL_PATH, REVERSE_LABELS_MAP, FEATURES_COLUMNS
+from constants import DT_MODEL_PATH, FEATURES_COLUMNS
 from utils import (
     populate_test_results_table,
     D_value_compute,
@@ -21,29 +23,26 @@ import pandas as pd
 import numpy as np
 
 
-def start_server(host="0.0.0.0", port=8440, pager_port=8441):
+def start_server(mllp_address, pager_address, debug=False):
     """
     Starts the TCP server to listen for incoming MLLP messages on the specified port.
     """
-    latencies = []  # to measure latency
-    outputs = []  # to measure f3 score
+    if debug:
+        latencies = []  # to measure latency
+        outputs = []  # to measure f3 score
+        count = 0
+    mllp_host, mllp_port = strip_url(mllp_address)
     # Initialise the in-memory database
-    db = InMemoryDatabase()
-    # print(db)
+    db = InMemoryDatabase()  # this also loads the previous history
     assert db != None, "In-memory Database is not initialised properly..."
-
-    # Populate the in-memory database with processed historical data
-    # populate_test_results_table(db, "history.csv")
 
     # Load the model once for use through out
     dt_model = load(DT_MODEL_PATH)
     assert dt_model != None, "Model is not loaded properly..."
-    count = 0
-    count1 = 0
     # Start the server
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((host, port))
-        print(f"Connected to simulator on {host}:{port}")
+        sock.connect((mllp_host, int(mllp_port)))
+        print(f"Connected to simulator on {mllp_address}")
 
         while True:
             data = sock.recv(1024)
@@ -54,9 +53,7 @@ def start_server(host="0.0.0.0", port=8440, pager_port=8441):
             hl7_data = process_mllp_message(data)
             if hl7_data:
                 message = parse_hl7_message(hl7_data)
-                # print("Parsed HL7 Message:")
-                # print(message)
-                # print(type(message))
+
                 category, mrn, data = parse_system_message(
                     message
                 )  # category is type of system message and data consists of age sex if PAS admit or date of blood test and creatanine result
@@ -69,9 +66,8 @@ def start_server(host="0.0.0.0", port=8440, pager_port=8441):
                     start_time = datetime.now()
                     patient_history = db.get_patient_history(str(mrn))
                     if len(patient_history) != 0:
-                        print(f"patient {mrn} has history... ")
-                        print("calculating features...")
-                        count = count + 1
+                        if debug:
+                            count = count + 1
                         latest_creatine_result = data[1]
                         latest_creatine_date = data[0]
                         D = D_value_compute(
@@ -95,18 +91,17 @@ def start_server(host="0.0.0.0", port=8440, pager_port=8441):
                             True,
                             D,
                         ]
-                        print("features crafted...")
                         input = pd.DataFrame([features], columns=FEATURES_COLUMNS)
                         aki = predict_with_dt(dt_model, input)
                         if aki[0] == "y":
-                            outputs.append((mrn, latest_creatine_date))
-                            print("Calling pager for mrn:", mrn)
-                            send_pager_request(mrn)
+                            if debug:
+                                outputs.append((mrn, latest_creatine_date))
+                            send_pager_request(mrn, pager_address)
                     end_time = datetime.now()
                     db.insert_test_result(mrn, data[0], data[1])
-                    latency = end_time - start_time
-                    latencies.append(latency)
-                    print(latency)
+                    if debug:
+                        latency = end_time - start_time
+                        latencies.append(latency)
 
                 # Create and send ACK message
                 ack_message = create_acknowledgement()
@@ -114,33 +109,52 @@ def start_server(host="0.0.0.0", port=8440, pager_port=8441):
             else:
                 print("No valid MLLP message received.")
 
-    # print("No data", count)
-    print("Patients with Historical Data", count)
+    if debug:
+        print("Patients with Historical Data", count)
 
-    # Calculate latency metrics
-    print(latencies)
-    mean_latency = np.mean(latencies)
-    median_latency = np.median(latencies)
-    min_latency = np.min(latencies)
-    max_latency = np.max(latencies)
-    percentile_99 = np.percentile(latencies, 99)
+        # Calculate latency metrics
+        mean_latency = np.mean(latencies)
+        median_latency = np.median(latencies)
+        min_latency = np.min(latencies)
+        max_latency = np.max(latencies)
+        percentile_99 = np.percentile(latencies, 99)
 
-    metrics = {
-        "Mean": mean_latency,
-        "Median": median_latency,
-        "Minimum": min_latency,
-        "Maximum": max_latency,
-        "99% Efficiency": percentile_99,
-    }
-    print(metrics)
+        metrics = {
+            "Mean": mean_latency,
+            "Median": median_latency,
+            "Minimum": min_latency,
+            "Maximum": max_latency,
+            "99% Efficiency": percentile_99,
+        }
+        print(metrics)
 
-    df = pd.DataFrame(outputs, columns=["mrn", "date"])
-    df["date"] = pd.to_datetime(df["date"]).dt.strftime("%Y-%m-%d %H:%M:%S")
-    df.to_csv("aki_predicted.csv", index=False)
+        df = pd.DataFrame(outputs, columns=["mrn", "date"])
+        df["date"] = pd.to_datetime(df["date"]).dt.strftime("%Y-%m-%d %H:%M:%S")
+        df.to_csv("aki_predicted.csv", index=False)
 
 
 def main():
-    start_server()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--mllp",
+        default="0.0.0.0:8440",
+        type=str,
+        help="Port on which to get HL7 messages via MLLP",
+    )
+    parser.add_argument(
+        "--pager",
+        default="0.0.0.0:8441",
+        type=str,
+        help="Post on which to send pager requests via HTTP",
+    )
+    parser.add_argument(
+        "--debug",
+        default=False,
+        type=bool,
+        help="Whether to calculate F3 and Latency Score",
+    )
+    flags = parser.parse_args()
+    start_server(flags.mllp, flags.pager, flags.debug)
 
 
 if __name__ == "__main__":
